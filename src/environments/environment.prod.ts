e557e4e286380717e35c9e47aa9066cc361fb472
Shahariar Suvin <shahariertanvir@gmail.com>
2020-12-06 21:56:50 +0600

    First commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyC4ADXpHtg9tZjD_rWHhpMWprEMzNnHcuk",
+    authDomain: "fitness-edecf.firebaseapp.com",
+    projectId: "fitness-edecf",
+    storageBucket: "fitness-edecf.appspot.com",
+    messagingSenderId: "690276279647",
+    appId: "1:690276279647:web:819438326649107cd042ea",
+    measurementId: "G-MF2EWFZ5B6"
+  }
+};
---
+++ src/environments/environment.ts
@@ -0,0 +1,17 @@
+// The file contents for the current environment will overwrite these during build.
+// The build system defaults to the dev environment which uses `environment.ts`, but if you do
+// `ng build --env=prod` then `environment.prod.ts` will be used instead.
+// The list of which env maps to which file can be found in `.angular-cli.json`.
+
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyC4ADXpHtg9tZjD_rWHhpMWprEMzNnHcuk",
+    authDomain: "fitness-edecf.firebaseapp.com",
+    projectId: "fitness-edecf",
+    storageBucket: "fitness-edecf.appspot.com",
+    messagingSenderId: "690276279647",
+    appId: "1:690276279647:web:819438326649107cd042ea",
+    measurementId: "G-MF2EWFZ5B6"
+  }
+};
